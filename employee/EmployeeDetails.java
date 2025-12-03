0c98f1a47d7e104cbd18bed03f3715deed6c2884
Hemalatha J <34550279+HemalathaSathish@users.noreply.github.com>
2025-12-03 19:55:47 +0530

    EmployeeDetails.java

--- employee/EmployeeDetails.java
+++ employee/EmployeeDetails.java
@@ -5,23 +5,24 @@ public class EmployeeDetails {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String Name = "Pranav";
-		int Age = 25;
-		int Salary = 25000;
-		String Designation = "Engineer";
-		boolean Working = true;
-		float Yearly_hike = 8.93f;
-		String Mobile_number = "9875623436";
-		char Grade = 'B';
+		String name = "Pranav";
+		int age = 25;
+		int salary = 25000;
+		String designation = "Engineer";
+		boolean working = true;
+		float yearly_hike = 8.93f;
+		String mobile_number = "9875623436";
+		char grade = 'B';
 		
-		System.out.println ("Employee Name : " +Name);
-		System.out.println ("Employee Age : " +Age);
-		System.out.println ("Employee Salary : " +Salary);
-		System.out.println ("Employee Designation : " +Designation);
-		System.out.println ("Employee Currently Working : " +Working);
-		System.out.println ("Employee Yearly Hike Percentage : " +Yearly_hike+ "%");
-		System.out.println ("Employee Grade Level : " +Grade);
-		System.out.println ("Employee Mobile Number : " +Mobile_number);
+		System.out.println ("Employee Name : " +name);
+		System.out.println ("Employee Age : " +age);
+		System.out.println ("Employee Salary : " +salary);
+		System.out.println ("Employee Designation : " +designation);
+		System.out.println ("Employee Currently Working : " +working);
+		System.out.println ("Employee Yearly Hike Percentage : " +yearly_hike+ "%");
+		System.out.println ("Employee Grade Level : " +grade);
+		System.out.println ("Employee Mobile Number : " +mobile_number);
 	}
 
 }
+
