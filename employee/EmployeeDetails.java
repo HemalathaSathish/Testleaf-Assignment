b86b6b051ecbc760694017be0de27134a17c7987
Hemalatha J <34550279+HemalathaSathish@users.noreply.github.com>
2025-12-03 16:06:26 +0530

    Add files via upload
    Day 1 : DataTypes Home Assignment

---
+++ employee/EmployeeDetails.java
@@ -0,0 +1,27 @@
+package employee;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Name = "Pranav";
+		int Age = 25;
+		int Salary = 25000;
+		String Designation = "Engineer";
+		boolean Working = true;
+		float Yearly_hike = 8.93f;
+		String Mobile_number = "9875623436";
+		char Grade = 'B';
+		
+		System.out.println ("Employee Name : " +Name);
+		System.out.println ("Employee Age : " +Age);
+		System.out.println ("Employee Salary : " +Salary);
+		System.out.println ("Employee Designation : " +Designation);
+		System.out.println ("Employee Currently Working : " +Working);
+		System.out.println ("Employee Yearly Hike Percentage : " +Yearly_hike+ "%");
+		System.out.println ("Employee Grade Level : " +Grade);
+		System.out.println ("Employee Mobile Number : " +Mobile_number);
+	}
+
+}
